200cda19c1e7554887b212c4264d2f287f990e0b
Dhruvang Choudhari <dhruvangsc@gmail.com>
2022-10-09 16:32:15 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ import {
                 variant="h6"
                 className={classes.title}
               >
-                Crypto Hunter
+                Crypto Explorer
               </Typography>
               <Select
                 variant="outlined"
